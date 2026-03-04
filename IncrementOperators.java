ba0bc197b8a13bb64b959ea718b68f3760570fa8
aranya-2005 <aranmurali2005@gmail.com>
2026-03-04 10:27:14 +0530

    java

---
+++ IncrementOperators.java
@@ -0,0 +1,20 @@
+class IncrementOperators{
+	public static void main(String args[]){
+		int x=10;
+		System.out.println(x);
+		x=x+1;
+		System.out.println(x);
+		x+=1;
+		System.out.println(x);
+		x++; //post-increment
+		System.out.println(x);
+		++x; //pre-increment
+		System.out.println(x);
+		
+		System.out.println(++x);
+		System.out.println(x++);
+		System.out.println(x);
+		
+		
+	}
+}
---
+++ OperatorsCombination.java
@@ -0,0 +1,22 @@
+class OperatorsCombination{
+	public static void main(String args[]){
+		int a=10;
+		int b=3;
+		int c=5;
+		
+		a=b++ + c++;
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+		System.out.println("c:"+c);
+		
+		c=b=a;
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+		System.out.println("c:"+c);
+		
+		b=a-- + --b - c++;
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+		System.out.println("c:"+c);
+	}
+}
