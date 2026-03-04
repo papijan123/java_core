5673ce550a3b6dbddd691a7c4306b767df993654
aranya-2005 <aranmurali2005@gmail.com>
2026-03-04 15:19:49 +0530

    Create Switchcase.java

---
+++ 04.03.2026/Switchcase.java
@@ -0,0 +1,33 @@
+class Switchcase{
+	public static void  main(String args[]){
+		int day=5;
+		
+		switch (day){
+		
+		case 1:
+			System.out.println("mon");
+			
+		case 2:
+			System.out.println("tue");
+			 
+		case 3:
+			System.out.println("wed");
+			
+		case 4:
+			System.out.println("thu");
+			
+		case 5:
+			System.out.println("fri");
+			
+		case 6:
+			System.out.println("sat");
+			
+		case 7:
+			System.out.println("sun");
+			
+		default:
+			System.out.println("Enter a valid number");
+			
+		}	
+	}
+}
