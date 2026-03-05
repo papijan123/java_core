c6b61e890cd260256871fe4ff4d839473c1af594
aranya-2005 <aranmurali2005@gmail.com>
2026-03-05 11:46:51 +0530

    java

---
+++ 05.03.2026/DoWhile.java
@@ -0,0 +1,12 @@
+class DoWhile{
+	public static void main(String args[]){
+		int x=1;
+		
+		
+		
+		do{
+			System.out.println(x);
+			x++;
+		}while (x<=10);
+	}
+}
---
+++ 05.03.2026/NestedDoWhile.java
@@ -0,0 +1,19 @@
+class NestedDoWhile{
+	public static void main(String args[]){
+		int x=1;
+		
+		
+		
+		do{
+			int y=1;
+			do{
+				System.out.print(y);
+				y++;
+			}while (y<=3);
+			
+			System.out.println(x);
+			x++;
+		}while (x<=5);
+		
+	}
+}
